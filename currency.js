fabe67034d07292e04cb596b8fc3acf5bb39b277
Kamila <k.nawrocka@lemonova.pl>
2023-09-25 17:18:22 +0200

    add API

--- currency.js
+++ currency.js
@@ -1,1 +1,25 @@
-console.log('currency')
+document.addEventListener('DOMContentLoaded', function (){
+
+    document.querySelector('form').onsubmit = function (){
+
+        fetch('http://api.exchangeratesapi.io/latest?base=USD').then(response => response.json())
+        .then(data => {
+            const currency = document.querySelector('#currency').value.toUpperCase();
+            const rate = data.rates[currency];
+            if (rate !== undefind){
+                document.querySelector('#result').innerHTML = rate;
+            } else{
+                document.querySelector('#result').innerHTML ="Invalid currency";
+            }
+        })
+        
+        .catch(error => {
+            console.log("Error", error);
+        });
+
+        return false;
+
+    }
+
+    
+});
